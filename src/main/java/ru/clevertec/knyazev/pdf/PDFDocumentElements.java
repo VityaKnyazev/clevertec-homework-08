655f3ef92fa2f38c27cd01fb0454571339e98b82
Vitya Knyazev <Utopio@tut.by>
2023-11-27 14:56:25 +0300

    Added abstraction for creating pdf document with data.

---
+++ src/main/java/ru/clevertec/knyazev/pdf/PDFDocumentElements.java
@@ -0,0 +1,28 @@
+package ru.clevertec.knyazev.pdf;
+
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.Table;
+
+/**
+ * Represents simple methods for creating pdf elements (paragraph, table, etc.)
+ * and filling it with data
+ */
+public sealed interface PDFDocumentElements permits PDFDocument {
+
+    default Paragraph createParagraph(String text) {
+        Paragraph paragraph = new Paragraph(text);
+        paragraph.setFontSize(14f);
+        return paragraph;
+    }
+
+    default Table createTable(float[] columnWidth) {
+        Table table = new Table(columnWidth);
+        table.setFontSize(14f);
+        return table;
+    }
+
+    default Table addCell(String cellText, Table table) {
+        table.addCell(cellText);
+        return table;
+    }
+}
